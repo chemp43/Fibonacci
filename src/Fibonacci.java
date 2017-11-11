d426bc52527864c12c6544843b6686947e5fd159
chemp43 <33561751+chemp43@users.noreply.github.com>
2017-11-10 19:44:56 -0700

    Added comments
    Added comments

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -10,9 +10,9 @@ public class Fibonacci {
         System.out.print("Enter where in the series to display: ");
         n = sc.nextInt();
         for (int i = 0; i < n; i++) {
-            j = k;
-            k = l;
-            l = j + k;
+            j = k; //j takes on k value (first loop j = 0)
+            k = l; //k takes on k value (first loop k = 0 then 1)
+            l = j + k; //adds j and k values to l (first loop 0+1=1)
             }
         System.out.print("The number in the " + n + " place of the series is: " + j);
     }
